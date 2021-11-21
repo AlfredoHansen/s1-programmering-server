129415df25e35188889d867331ff76fd0ab7de83
AlfredoHansen <alfr0129@gmail.com>
2021-11-21 18:06:32 +0100

    Tilføjet filter funktionalitet på produkter

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,9 @@ const fs = require('fs');
 
 //Route /products - hent alle products
 router.get('/api/products', function(req, res, next) {
+  
+  let category = req.query.category;
+  
   //Denne linie kigger på om filen exsistere
   var file_exist = fs.existsSync('products.json');
   //Tjek her om filen faktisk eksitere
@@ -11,9 +14,33 @@ router.get('/api/products', function(req, res, next) {
     //Hent data fra filen
     var data = fs.readFileSync('products.json');
     //Konverter vores data til json
-    obj = JSON.parse(data);
+    products = JSON.parse(data);
     //Returner json til klienten
-    res.send(obj);
+
+    //Hvis category ikke er med i requestet til serveren, så skal vi bare returnere alle produkter
+    if(!category) {
+
+      res.send(products);
+
+    } else {
+      //Her ved vi at category er sat, så derfor filtere vi..
+
+
+      var filteredProducts = [];
+
+      products.forEach(product => {
+        
+        //Tag kun de produkter som passer på vores kategori
+        if(product.category == category) {
+          filteredProducts.push(product) 
+        }
+
+      });
+      
+      res.send(filteredProducts);
+
+    }
+
   } else {
     //Hvis ikke så fortæl brugeren at den ikke findes
     res.send('File not found!');

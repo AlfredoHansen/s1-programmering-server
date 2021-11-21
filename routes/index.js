73b4ae1d810dbbb48821ae4866169526ca3ab4ba
AlfredoHansen <alfr0129@gmail.com>
2021-11-21 17:15:31 +0100

    Tilføjet muligheden for at hente produkter

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+const fs = require('fs');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+//Route /products - hent alle products
+router.get('/api/products', function(req, res, next) {
+  //Denne linie kigger på om filen exsistere
+  var file_exist = fs.existsSync('products.json');
+  //Tjek her om filen faktisk eksitere
+  if (file_exist) {
+    //Hent data fra filen
+    var data = fs.readFileSync('products.json');
+    //Konverter vores data til json
+    obj = JSON.parse(data);
+    //Returner json til klienten
+    res.send(obj);
+  } else {
+    //Hvis ikke så fortæl brugeren at den ikke findes
+    res.send('File not found!');
+  }
 });
 
 module.exports = router;
